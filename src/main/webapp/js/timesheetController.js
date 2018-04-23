0cde543e14d3a83a131058584f93a5fb0242d1fb
Hrishikesh Deshpande <hvdandroid@gmail.com>
2018-04-22 21:41:55 -0400

    Add files via upload

--- src/main/webapp/js/timesheetController.js
+++ src/main/webapp/js/timesheetController.js
@@ -1,1 +1,83 @@
+var tsApp = angular.module('tsApp',['ui.bootstrap']) ;
 
+tsApp.controller('tsAppCtrl',function($scope,$http) {
+	$scope.dt = new Date() ;
+	$scope.currentDay = $scope.dt.getDate() - 1 ;
+	$scope.disableWeekends = function(data) {
+	    var date = data.date,
+	      mode = data.mode;
+	    return mode === 'day' && (date.getDay() === 0 || date.getDay() === 6);
+	  }
+	$scope.options = {maxDate:new Date()
+	,minDate: new Date($scope.dt.getFullYear(), $scope.dt.getMonth(), 1),
+	dateDisabled:$scope.disableWeekends
+	}
+	 
+	
+	$scope.days = [] ;
+	$scope.createDaysArray = function() {
+		for(var i=0 ; i<$scope.dt.getDate() ; i++) {
+		$scope.days.push({
+			date:new Date($scope.dt.getFullYear(), $scope.dt.getMonth(), i+1),
+			tasks:[],
+			hours:0,
+			
+		  }) ;	
+		}
+	}
+	$scope.createDaysArray() ;
+	
+	
+	
+	$scope.title = ["Sr.No","Task Name","Task Details", "Hours","Delete"] ;
+	$scope.addTask = function(day) {
+		var hrdiff = 1 ;
+		hrdiff = 8 - day.hours ;
+		if(hrdiff <= 0) {
+			return ;
+		}
+		var task = {
+				name:"",
+				details:"",
+				hours:1
+					
+		}
+		
+	$scope.days[$scope.currentDay].tasks.push(task)	;
+	$scope.days[$scope.currentDay].hours+=1 ;
+	}
+	
+	$scope.onDateChange = function(dt) {
+		$scope.currentDay = dt.getDate() - 1 ;
+	}
+	
+	$scope.onHoursChange = function(days,tasks,index,oldHours) {
+		var sum = 0 ;
+		oldHours = Number(oldHours)
+		var t = days[$scope.currentDay].tasks ;
+		
+		for (var i=0 ; i<t.length ; i++) {
+			
+			sum+=t[i].hours ;
+			if(sum > 8) {
+				$scope.days[$scope.currentDay].tasks[index].hours = oldHours ;
+				return ;
+			}
+		}
+		$scope.days[$scope.currentDay].hours += $scope.days[$scope.currentDay].tasks[index].hours - oldHours
+	}
+	
+	$scope.removeTask = function(taskList , index) {
+		taskList.splice(index,1) ;
+	}
+	
+	$scope.onClickOfSubmit = function(days) {
+		$http.post('/saveTimesheet').then(function(response){
+			alert(response.data.response) ;
+		},function(response){
+			alert(response.data.response); 
+		})
+	}
+	
+	
+}) ;
