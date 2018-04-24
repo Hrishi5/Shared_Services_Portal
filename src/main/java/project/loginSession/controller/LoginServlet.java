f613b2d99448cdf53268c4fe1235e038de49fabe
harshals25 <35043807+harshals25@users.noreply.github.com>
2018-04-24 13:29:16 -0400

    Add files via upload

---
+++ src/main/java/project/loginSession/controller/LoginServlet.java
@@ -0,0 +1,84 @@
+package project.loginSession.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import project.loginSession.dto.LoginDTO;
+import project.loginSession.dao.*; 
+ 
+public class LoginServlet extends HttpServlet {
+private static final long serialVersionUID = 1L;
+ 
+public LoginServlet() {
+}
+ 
+protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException 
+{
+ String userName = request.getParameter("username");
+ String password = request.getParameter("password");
+ 
+ LoginDTO loginDto = new LoginDTO();
+ 
+ loginDto.setUserName(userName);
+ loginDto.setPassword(password);
+ 
+ LoginDao loginDao = new LoginDao();
+ 
+ try
+ {
+ String userValidate = loginDao.authenticateUser(loginDto);
+ 
+ if(userValidate.equals("Admin_Role"))
+ {
+ System.out.println("Admin's Home");
+ 
+ HttpSession session = request.getSession(); //Creating a session
+ session.setAttribute("Admin", userName); //setting session attribute
+ request.setAttribute("userName", userName);
+ 
+ request.getRequestDispatcher("/JSP/Admin.jsp").forward(request, response);
+ }
+ else if(userValidate.equals("Editor_Role"))
+ {
+ System.out.println("Editor's Home");
+ 
+ HttpSession session = request.getSession();
+ session.setAttribute("Editor", userName);
+ request.setAttribute("userName", userName);
+ 
+ request.getRequestDispatcher("/JSP/Editor.jsp").forward(request, response);
+ }
+ else if(userValidate.equals("User_Role"))
+ {
+ System.out.println("User's Home");
+ 
+ HttpSession session = request.getSession();
+ session.setMaxInactiveInterval(10*60);
+ session.setAttribute("Student", userName);
+ request.setAttribute("userName", userName);
+ 
+ request.getRequestDispatcher("/JSP/User.jsp").forward(request, response);
+ }
+ else
+ {
+ System.out.println("Error message = "+userValidate);
+ request.setAttribute("errMessage", userValidate);
+ 
+ request.getRequestDispatcher("/JSP/Login.jsp").forward(request, response);
+ }
+ }
+ catch (IOException e1)
+ {
+ e1.printStackTrace();
+ }
+ catch (Exception e2)
+ {
+ e2.printStackTrace();
+ }
+} //End of doPost()
+}
---
+++ src/main/java/project/loginSession/controller/LogoutServlet.java
@@ -0,0 +1,5 @@
+package project.loginSession.controller;
+
+public class LogoutServlet {
+
+}
