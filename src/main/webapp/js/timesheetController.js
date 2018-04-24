d8ab053d9f7876ddd9c215445c68e37c39105c2f
Hrishi5 <hvdandroid@gmail.com>
2018-04-23 21:43:23 -0400

    Timesheet UI

--- src/main/webapp/js/timesheetController.js
+++ src/main/webapp/js/timesheetController.js
@@ -1,6 +1,7 @@
 var tsApp = angular.module('tsApp',['ui.bootstrap']) ;
 
-tsApp.controller('tsAppCtrl',function($scope,$http) {
+tsApp.controller('tsAppCtrl',function($scope,$http,$filter) {
+	$scope.burl = 'Shared_Services_Portal' ;
 	$scope.dt = new Date() ;
 	$scope.currentDay = $scope.dt.getDate() - 1 ;
 	$scope.disableWeekends = function(data) {
@@ -21,7 +22,7 @@ tsApp.controller('tsAppCtrl',function($scope,$http) {
 			date:new Date($scope.dt.getFullYear(), $scope.dt.getMonth(), i+1),
 			tasks:[],
 			hours:0,
-			
+			checkedDb:false
 		  }) ;	
 		}
 	}
@@ -29,7 +30,7 @@ tsApp.controller('tsAppCtrl',function($scope,$http) {
 	
 	
 	
-	$scope.title = ["Sr.No","Task Name","Task Details", "Hours","Delete"] ;
+	$scope.title = ["Sr.No","Task Name","Hours","Delete"] ;
 	$scope.addTask = function(day) {
 		var hrdiff = 1 ;
 		hrdiff = 8 - day.hours ;
@@ -49,6 +50,19 @@ tsApp.controller('tsAppCtrl',function($scope,$http) {
 	
 	$scope.onDateChange = function(dt) {
 		$scope.currentDay = dt.getDate() - 1 ;
+		if($scope.days[$scope.currentDay].tasks.length === 0 
+				&& $scope.days[$scope.currentDay].checkedDb === false) {
+			$http.post('/Shared_Services_Portal/getTimesheet',
+					{key:'1',value:$filter('date')(new Date($scope.dt),'yyyy-MM-dd')})
+			.then(function(response){
+				if(response.data.status === '1') {
+					$scope.days[$scope.currentDay] = response.data.response ;
+					$scope.days[$scope.currentDay].checkedDb === true ;
+				}
+			},function(response){
+				
+			}) ;
+		}
 	}
 	
 	$scope.onHoursChange = function(days,tasks,index,oldHours) {
@@ -72,7 +86,26 @@ tsApp.controller('tsAppCtrl',function($scope,$http) {
 	}
 	
 	$scope.onClickOfSubmit = function(days) {
-		$http.post('/saveTimesheet').then(function(response){
+		var data = [] ;
+		var updatedData = [] ;
+		for(var i=0 ; i<days.length ; i++) {
+			if(days[i].tasks.length > 0 && days[i].hours > 0 && days[i].checkedDb === false) {
+			var tasks = days[i].tasks
+			for(var j=0 ; j<tasks.length ; j++) {
+			 if(tasks[j].name.trim() === '' || tasks[j].hours===0) {	
+				 $scope.removeTask(days[i].tasks,j) ;
+				 continue ;
+			 }
+			}
+			if(days[i].checkedDb === false) {
+			data.push(days[i]) ;
+			}else{
+				updatedData.push(days[i]) ;
+			}
+		}
+		}
+		console.log(JSON.stringify(data))
+		$http.post('/Shared_Services_Portal/saveTimesheet',data).then(function(response){
 			alert(response.data.response) ;
 		},function(response){
 			alert(response.data.response); 
